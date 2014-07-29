27a513ed9fddfda990db12ef694605f53f833119
Jijeesh K A <silentheartbeat@gmail.com>
2014-07-29 14:52:14 +0530

    Create javaservice.sh
    This is used to create a jar file as a service in linux

---
+++ javaservice.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+# description: javaservice Start Stop Restart 
+# processname: javaservice
+# chkconfig: 234 90 60 
+#
+# author Jijeesh K A
+# This is used for creating a jar file as a service in linux
+# CentOS use chkconfig --add javaservice
+# service javaservice start
+SERVICE_NAME=MyJavaService
+PATH_TO_JAR=/shell/javaservice.jar
+PID_PATH_NAME=/tmp/MyService-pid
+#java -cp foo.jar full.package.name.ClassName 
+case $1 in
+    start)
+        echo "Starting $SERVICE_NAME ..."
+        if [ ! -f $PID_PATH_NAME ]; then
+            nohup java -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+                        echo $! > $PID_PATH_NAME
+            echo "$SERVICE_NAME started ..."
+        else
+            echo "$SERVICE_NAME is already running ..."
+        fi
+    ;;
+    stop)
+        if [ -f $PID_PATH_NAME ]; then
+            PID=$(cat $PID_PATH_NAME);
+            echo "$SERVICE_NAME stoping ..."
+            kill $PID;
+            echo "$SERVICE_NAME stopped ..."
+            rm $PID_PATH_NAME
+        else
+            echo "$SERVICE_NAME is not running ..."
+        fi
+    ;;
+    restart)
+        if [ -f $PID_PATH_NAME ]; then
+            PID=$(cat $PID_PATH_NAME);
+            echo "$SERVICE_NAME stopping ...";
+            kill $PID;
+            echo "$SERVICE_NAME stopped ...";
+            rm $PID_PATH_NAME
+            echo "$SERVICE_NAME starting ..."
+            nohup java -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+                        echo $! > $PID_PATH_NAME
+            echo "$SERVICE_NAME started ..."
+        else
+            echo "$SERVICE_NAME is not running ..."
+        fi
+    ;;
+esac
